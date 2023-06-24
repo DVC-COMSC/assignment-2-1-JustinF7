e53cab81d2fb11172e61b00e071288d782b1574b
JustinF7 <fu.justin3@gmail.com>
2023-06-23 18:24:21 -0700

    Male Female %

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 def main():
-    """
-    ##################################################
-    Comlete your code here
-    Use m_perc and f_perc for your results
-    ##################################################
-    """
-
-    """
-    ########################################
-    # Do not delete the return statement
-    ########################################
-    """
+    male = float(input("Number of Males - "))
+    female = float(input("Number of Females - "))
+    total = male + female
+    m_perc = str(round(male/total * 100))
+    f_perc = str(round(female/total * 100))
+    
+    print("Male Percentage - " + m_perc + "%")
+    print("Female Percentage - " + f_perc + "%")
     return m_perc, f_perc
+
+
 
 
 if __name__ == '__main__':
     main()
+
