7f450099311437d99e6099c8dd459fe064e01947
JustinF7 <fu.justin3@gmail.com>
2023-06-23 18:46:19 -0700

    female male percent

--- male female p.py
+++ male female p.py
@@ -2,11 +2,11 @@ def main():
     male = float(input("Number of Males - "))
     female = float(input("Number of Females - "))
     total = male + female
-    m_perc = str(round(male/total * 100))
-    f_perc = str(round(female/total * 100))
+    m_perc = round(male/total * 100, 4)
+    f_perc = round(female/total * 100, 4)
     
-    print("Male Percentage - " + m_perc + "%")
-    print("Female Percentage - " + f_perc + "%")
+    print("Male Percentage - " + str(m_perc) + "%")
+    print("Female Percentage - " + str(f_perc) + "%")
     return m_perc, f_perc
 
 
